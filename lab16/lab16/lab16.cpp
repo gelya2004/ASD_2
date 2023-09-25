009d3ef673894a1818e59eb2db8b8652258db7cc
gelya2004 <95101373+gelya2004@users.noreply.github.com>
2023-09-25 13:56:15 +0200

    Add files via upload

---
+++ lab16/lab16/lab16.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int W = 7;
+	pair<int, int> things[] = { (make_pair(1, 2500)),make_pair(3, 2000),make_pair(3, 2000),make_pair(3, 2000),make_pair(4, 4000) ,make_pair(8, 4000) };
+
+	int** c = new int* [size(things) + 1];
+	for (int i = 0; i <= size(things); i++)
+		c[i] = new int[W + 1];
+
+	for (int i = 0; i <= size(things); i++)
+	{
+		for (int j = 0; j <= W; j++)
+		{
+			if (i == 0 || j == 0)
+				c[i][j] = 0;
+
+			else
+			{
+				if (things[i - 1].first > j)
+					c[i][j] = c[i - 1][j];
+
+				else
+					c[i][j] = max(c[i - 1][j], things[i - 1].second + c[i - 1][j - things[i - 1].first]);
+
+			}
+		}
+	}
+
+	for (int i = 0; i <= size(things); i++)
+	{
+		for (int j = 0; j <= W; j++)
+		{
+			if (i == 0 || j == 0)
+				cout << " \t";
+			else
+				cout << c[i][j] << " \t \t";
+		}
+		cout << endl;
+	}
+	cout << "Итоговая стоимость: " << c[size(things)][W];
+
+	return 0;
+}
---
+++ lab17/lab17/lab17.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+float weight(vector<float> basket)
+{
+	float count = 0;
+	for (int i = 0; i < basket.size(); i++)
+		count += basket[i];
+	return count;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	cout << "Какой вес ящиков? ";
+	int W;	cin >> W;
+
+	cout << "Сколько вещей? ";
+	int count;	cin >> count;
+
+	
+
+	cout << "Вводите вес передметов. \n";
+	vector <float> things;
+	float a; 
+	while (things.size() < count)
+	{
+		cin >> a;
+		if (a < 0)
+		{
+			cout << "Отрицательный вес!!! :(";
+			exit(1);
+		}
+		things.push_back(a);
+	}
+
+	for (int i = 0; i < things.size(); i++)
+	{
+		if (things[i] > W)
+		{
+			things.erase(things.begin() + i);
+			i--;
+		}
+	}
+		
+
+	vector<vector<float>> baskets;
+
+	while (!things.empty())
+	{
+		for (int i = 0; i < things.size(); i++)
+		{
+			vector<float> basket;
+			while (weight(basket) <= W)
+			{
+				if (weight(basket) + things[i] <= W)
+				{
+					basket.push_back(things[i]);
+					things.erase(things.begin() + i);
+					i--;
+				}
+				i++;
+				if (i >= things.size())
+					break;
+			}
+			baskets.push_back(basket);
+			basket.clear();
+		}
+	}
+	cout << "\n \nНужно " << baskets.size() << " ящиков \n";
+	for (int i = 0; i < baskets.size(); i++)
+	{
+		cout << i + 1 << ":";
+		for (int j=0;j<baskets[i].size();j++)
+			cout << baskets[i][j] << " ";
+		cout << endl;
+	}
+	return 0;
+}
