35d65e80670e1cc52cf92a35b57b6fce26464386
gelya2004 <95101373+gelya2004@users.noreply.github.com>
2023-09-28 19:09:02 +0200

    Update main.cpp

--- lab18/main.cpp
+++ lab18/main.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-void Quick_Sort(int low, int high, vector<int> &x)
+void Quick_Sort(int low, int high, vector<int> &x) // Функция быстрой сортировки
 {
 	if (low >= high)
 	{
@@ -10,19 +10,19 @@ void Quick_Sort(int low, int high, vector<int> &x)
 	}
 	int m = (low + high) / 2 + 1;
 	int k = x[m];
-	int l = low - 1;
+	int l = low - 1;   // Инициализация индексов левого и правого подмассивов
 	int r = high + 1;
 	while (1)
 	{
 		do
 		{
-			l++;
+			l++;  // Поиск элемента больше опорного в левой части
 		} while (x[l] < k);
 		do
 		{
-			r--;
+			r--;     // Поиск элемента меньше опорного в правой части
 		} while (x[r] > k);
-		if (l >= r)
+		if (l >= r) // Если индексы пересеклись, прекращаем разделение
 		{
 			break;
 		}
@@ -33,12 +33,12 @@ void Quick_Sort(int low, int high, vector<int> &x)
 	Quick_Sort(low, l, x);
 	Quick_Sort(r, high, x);
 }
-bool SummSubb(vector<int> numbers, int sum)
+bool SummSubb(vector<int> numbers, int sum) // Функция для поиска подмассива, сумма элементов которого равна заданной сумме
 {
 	int i = 0, s=0;
 	vector<int>summ;
 	start:
-	if (s + numbers[i] <= sum)
+	if (s + numbers[i] <= sum) //если сумма текущих элементов и следующего элемента не превышает заданную сумму, то переходим к след элементу
 	{
 		s += numbers[i];
 		summ.push_back(numbers[i]);
@@ -50,7 +50,7 @@ bool SummSubb(vector<int> numbers, int sum)
 		}
 	}
 
-	if (s == sum)
+	if (s == sum)   // Если сумма равна заданной сумме, выводим на экран найденный подмассив
 	{ 
 		cout << "\n Sum is found! \n Sum:\n";
 		for (int i : summ)
